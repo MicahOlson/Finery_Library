cdf8db494b08bbcfea30e8b94c5c21df41d4faee
thorgrim88@gmail.com <thorgrim88@gmail.com>
2021-03-10 13:12:21 -0800

    add all passing tests for Patrons
    Co-authored-by: Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>

--- lib/patron.rb
+++ lib/patron.rb
@@ -22,4 +22,32 @@ class Patron
     self.name == patron_to_compare.name
   end
 
+  def save
+    result = DB.exec("INSERT INTO patrons (name) VALUES ('#{@name}') RETURNING id;")
+    @id = result.first["id"].to_i
+  end
+
+  def self.clear
+    DB.exec("DELETE FROM patrons *;")
+  end
+
+  def self.find(id)
+    patron = DB.exec("SELECT * FROM patrons WHERE id = #{id};").first
+    if patron 
+      name = patron["name"]
+      id = patron["id"].to_i
+      Patron.new({name: name, id: id})
+    else
+      nil
+    end
+  end
+
+  def update(name)
+    @name = name
+    DB.exec("UPDATE patrons SET name = '#{@name}' WHERE id = #{@id};")
+  end
+
+  def delete
+    DB.exec("DELETE FROM patrons WHERE id = #{@id};")
+  end
 end
--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -15,8 +15,54 @@ describe('#Patron') do
     end
   end
 
+  describe('#save') do
+    it("saves an patron") do
+      patron = Patron.new({name: "Matthew"})
+      patron.save
+      patron2 = Patron.new({name: "Julie"})
+      patron2.save
+      expect(Patron.all).to(eq([patron, patron2]))
+    end
+  end
 
+  describe('.clear') do
+    it("clears all patrons") do
+      patron = Patron.new({name: "Julie"})
+      patron.save
+      patron2 = Patron.new({name: "Matthew"})
+      patron2.save
+      Patron.clear
+      expect(Patron.all).to(eq([]))
+    end
+  end
 
+  describe('.find') do
+    it("finds an patron by id") do
+      patron = Patron.new({name: "Julie"})
+      patron.save
+      patron2 = Patron.new({name: "Matthew"})
+      patron2.save
+      expect(Patron.find(patron.id)).to(eq(patron))
+    end
+  end
 
+  describe('#update') do
+    it("updates an patron by id") do
+      patron = Patron.new({name: "Julie"})
+      patron.save
+      patron.update("Julie")
+      expect(patron.name).to(eq("Julie"))
+    end
+  end
 
+  describe('#delete') do
+    it("deletes an patron by id") do
+      patron = Patron.new({name: "Julie"})
+      patron.save
+      patron2 = Patron.new({name: "Matthew"})
+      patron2.save
+      patron.delete
+      expect(Patron.all).to(eq([patron2]))
+    end
+  end
 end
