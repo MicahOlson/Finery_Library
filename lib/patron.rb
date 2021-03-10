b66b3399c93d81f7c6374f60a492f3d81173e9c6
thorgrim88@gmail.com <thorgrim88@gmail.com>
2021-03-10 11:31:23 -0800

    add passing all test for patron
    Co-authored-by: Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>

--- lib/patron.rb
+++ lib/patron.rb
@@ -1,5 +1,21 @@
-
-
 class Patron
-  attr_reader :name, :id
+  attr_reader :id
+  attr_accessor :name
+
+  def intialize(attributes)
+    @name = attributes[:name]
+    @id = attributes[:id]
+  end
+
+  def self.all
+    returned_patrons = DB.exec("SELECT * FROM patrons;")
+    patrons = []
+    returned_patrons.each do |patron|
+      name = patron["name"]
+      id = patron["id"].to_i
+      patrons.push(Patron.new({name: name, id: id}))
+    end
+    patrons
+  end
+
 end
---
+++ spec/patron_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe('#Patron') do
+  describe('.all') do
+    it("returns an empty array when there are no patrons") do
+      expect(Patron.all).to(eq([]))
+    end
+  end
+
+
+
+
+
+
+
+end
